11c74c8b8e1914fc8bb3e9a9260a1737ac4ad43c
ramya72002 <nsriramya7@gmail.com>
2024-08-29 11:52:39 +0530

    filter button funtionality added

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ RETRY_DELAY_SECONDS = 1
 app = Flask(__name__)
 
 # Allow requests from specific origins
-CORS(app, origins=["https://chess-main.vercel.app","https://chessdemo-alpha.vercel.app","https://chessdemo-l3qrzgj5q-ramyas-projects-4cb2348e.vercel.app","https://chess-main-git-main-ramyas-projects-4cb2348e.vercel.app","http://localhost:3000"])
+CORS(app, origins=["https://chess-main.vercel.app","https://demo4-rose.vercel.app","https://demo4-rose.vercel.app/admin","https://chessdemo-alpha.vercel.app","https://chessdemo-l3qrzgj5q-ramyas-projects-4cb2348e.vercel.app","https://chess-main-git-main-ramyas-projects-4cb2348e.vercel.app","http://localhost:3000"])
 
 load_dotenv()
 
