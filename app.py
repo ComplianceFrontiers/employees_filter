853baabb6538c1b2e0651a08847d6128315e2ff2
ramya72002 <nsriramya7@gmail.com>
2024-08-29 12:14:11 +0530

    filter button funtionality added

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def filter_records():
 
         return jsonify(results), 200
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)}), 500 
     
 if __name__ == '__main__':
     app.run()
